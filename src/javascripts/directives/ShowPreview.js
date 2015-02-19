10eebbf57e5445d616e907d31908f3b51ada60dd
Jeramy <theceremony@gmail.com>
2015-02-18 19:37:30 -0500

    fixed a directive issue

---
+++ src/javascripts/directives/ShowPreview.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function(){
+	return function($scope,element,attrs) {
+		element.bind('mouseover',function($event){
+			$event.preventDefault();
+			console.log(attrs['showpreview']);
+		})
+	}
+}
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,38 +1,17 @@
 'use strict';
-window.$		= require('jquery');
+window.$			= require('jquery');
 
 var angular 		= require('angular'),
 	MenuCtrl 		= require('./controllers/MenuCtrl'),
-	ToggleVisible 	= require('./directives/ToggleVisible');
+	ToggleVisible 	= require('./directives/ToggleVisible'),
+	ShowPreview 	= require('./directives/ShowPreview');
  
 
 angular // Work Menu Module
 .module('ngAppStrict', [])
 	.controller('MenuCtrl', ['$scope','$http', MenuCtrl])
 	.directive('togglevisible',ToggleVisible)
-	.directive('show-project-preview', ['', function(){
-		// Runs during compile
-		return {
-			// name: '',
-			// priority: 1,
-			// terminal: true,
-			// scope: {}, // {} = isolate, true = child, false/undefined = no change
-			// controller: function($scope, $element, $attrs, $transclude) {},
-			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-			// restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
-			// template: '',
-			// templateUrl: '',
-			// replace: true,
-			// transclude: true,
-			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-			link: function($scope, iElm, iAttrs, controller) {
-				console.log(iElm);
-				iElm.bind('mouseover',function($event){
-					console.log(iAtters['show-project-preview']);
-				})
-			}
-		};
-	}]);
+	.directive('showpreview', ShowPreview);
 
 
 
