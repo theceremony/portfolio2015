4e7339303ccbc90e3a863d88f35555ee74a590eb
Jeramy Morrill <jeramymorrill@bossaimac01-jeramy.local>
2015-04-07 21:41:15 -0400

    small changes

--- src/javascripts/controllers/WorkCtrl.js
+++ src/javascripts/controllers/WorkCtrl.js
@@ -2,18 +2,6 @@
 
  var WorkCtrl = function($scope,$routeParams,$filter) {
 	console.log('WorkCtrl start');
-	// more soon
-	// ----------
-	//var obj = $filter('filter')($scope.projects, {projectId:$routeParams.projectId});
-
-	
-
-	
-
-	$scope.displayProjectById = function(id){
-		console.log(id);
-	}
-	console.log($scope.getJobById($routeParams.projectId));
 
 	$scope.title = "yo bro";
 };
